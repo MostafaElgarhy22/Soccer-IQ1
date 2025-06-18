bc7f51c45d41ef9b527ed981c1d694139029887c
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-06-19 00:30:51 +0300

    Create HealthController

---
+++ Soccer IQ/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Soccer_IQ.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("✅ API is healthy and running!");
+        }
+    }
+}
